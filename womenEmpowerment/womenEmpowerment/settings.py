2434390546e47ec484799169bc182624543a0a43
AniketShahane <shahaneaniket11@gmail.com>
2019-03-02 22:15:02 +0530

    Fix models

--- womenEmpowerment/womenEmpowerment/settings.py
+++ womenEmpowerment/womenEmpowerment/settings.py
@@ -26,7 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'organization',
-    'donors'
+    'donors',
+    'woman',
+    'message',
 ]
 
 MIDDLEWARE = [
