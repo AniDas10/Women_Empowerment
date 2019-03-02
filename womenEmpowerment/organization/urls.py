28cbdf60b57f32aa622f22d3d11ba45d6eec14da
dharmikparikh9934 <41770049+dharmikparikh9934@users.noreply.github.com>
2019-03-03 01:20:46 +0530

    Integration completed without Admin

--- womenEmpowerment/organization/urls.py
+++ womenEmpowerment/organization/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('all/', views.organisations, name='organisations'),
     path('<int:organization_id>/', views.organisation, name='organisaiton'),
     path('login/', views.login, name='organisation_login'),
-    path('register/', views.register, name='organisation_register')
+    path('register/', views.register, name='organisation_register'),
+    path('home/',views.homepage,name='home')
 ]
